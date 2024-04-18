70baebd53bc59760ad30b69f2be5507756465fa6
Hyeseong Kim <hey@hyeseong.kim>
2024-04-18 16:44:01 +0900

    fuzz via Intl adapter

--- fuzz/target.cjs
+++ fuzz/target.cjs
@@ -1,9 +1,16 @@
-const { graphemeSegments } = require('../grapheme.cjs');
+const assert = require('node:assert/strict');
+const { Segmenter } = require('unicode-segmenter/intl-adapter');
+
+let segmenter = new Segmenter();
+let intlSegmenter = new Intl.Segmenter();
 
 /**
  * @param {Buffer} data
  */
 module.exports.fuzz = (data) => {
   let fuzzerData = data.toString();
-  void [...graphemeSegments(fuzzerData)];
+  void assert.deepEqual(
+    [...segmenter.segment(fuzzerData)],
+    [...intlSegmenter.segment(fuzzerData)],
+  );
 };
