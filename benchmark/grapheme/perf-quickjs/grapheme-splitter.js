4d2fb0074dddc78751288df640c40c0a4f5fb327
Hyeseong Kim <hey@hyeseong.kim>
2024-11-24 10:30:43 +0900

    add quickjs benchmark

---
+++ benchmark/grapheme/perf-quickjs/grapheme-splitter.js
@@ -0,0 +1,28 @@
+import { inputs, simpleBench } from '../../_simple-bench.js';
+
+import { GraphemeSplitter } from '../bundle-entries/grapheme-splitter.bundle.js';
+
+let graphemeSplitter = new (GraphemeSplitter.default || GraphemeSplitter)();
+
+{
+  let result = simpleBench(1000, () => {
+    void [...graphemeSplitter.iterateGraphemes(inputs.small)];
+  });
+
+  print(`grapheme-splitter (small input)`);
+  print(`samples: ${result.samples}`);
+  print(`duration (avg): ${result.avgDuration}`);
+  print();
+}
+
+
+{
+  let result = simpleBench(1000, () => {
+    void [...graphemeSplitter.iterateGraphemes(inputs.medium)];
+  });
+
+  print(`grapheme-splitter (medium input)`);
+  print(`samples: ${result.samples}`);
+  print(`duration (avg): ${result.avgDuration}`);
+  print();
+}
