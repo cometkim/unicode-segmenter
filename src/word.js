fbf1927cac5daeb1f7e4ac6e5fb08de4779d3b3d
Nikita Karamov <me@kytta.dev>
2025-05-21 23:59:29 +0200

    Implement cat() for word breaks

--- src/word.js
+++ src/word.js
@@ -16,6 +16,78 @@
 
 // @ts-check
 
+import { findUnicodeRangeIndex } from "./core.js";
 import { WordCategory, word_ranges } from "./_word_data.js";
 
+/**
+ * @typedef {import('./_word_data.js').WC_Any} WC_Any
+ *
+ * @typedef {import('./_word_data.js').WordCategoryNum} WordCategoryNum
+ * @typedef {import('./_word_data.js').WordCategoryRange} WordCategoryRange
+ *
+ * @typedef {object} WordSegmentExtra
+ * @property {number} _hd The first code point of the segment
+ * @property {WordCategoryNum} _catBegin Beginning Word_Break category of the segment
+ * @property {WordCategoryNum} _catEnd Ending Word_Break category of the segment
+ *
+ * @typedef {import('./core.js').Segmenter<WordSegmentExtra>} WordSegmenter
+ */
+
 export { WordCategory };
+
+/**
+ * `Word_break` property value of a given codepoint
+ *
+ * @see https://www.unicode.org/reports/tr29/tr29-43.html#Default_Word_Boundaries
+ *
+ * @param {number} cp
+ * @param {import('./_word_data.js').WordCategoryRange} cache
+ * @return {WordCategoryNum}
+ */
+function cat(cp, cache) {
+  if (cp < 127) {
+    // Special-case optimization for ascii, except U+007F.  This
+    // improves performance even for many primarily non-ascii texts,
+    // due to use of punctuation and white space characters from the
+    // ascii range.
+    if (cp === 11 || cp === 12) {
+      return 13 /* WC_Newline */;
+    } else if (cp === 13) {
+      return 2 /* WC_CR */;
+    } else if (cp === 32) {
+      return 17 /* WC_WSegSpace */;
+    } else if (cp === 34) {
+      return 3 /* WC_Double_Quote */;
+    } else if (cp === 39) {
+      return 16 /* WC_Single_Quote */;
+    } else if (cp === 44 || cp === 59) {
+      return 11 /* WC_MidNum */;
+    } else if (cp === 46) {
+      return 12 /* WC_MidNumLet */;
+    } else if (cp >= 48 && cp <= 57) {
+      return 14 /* WC_Numeric */;
+    } else if (cp === 58) {
+      return 10 /* WC_MidLetter */;
+    } else if (cp === 95) {
+      return 5 /* WC_ExtendNumLet */;
+    } else if ((cp >= 65 && cp <= 90) || (cp >= 97 && cp <= 122)) {
+      return 5 /* WC_ExtendNumLet */;
+    } else {
+      return 0 /* WC_Any */;
+    }
+  } else {
+    // If this char isn't within the cached range, update the cache to the
+    // range that includes it.
+    if (cp < cache[0] || cp > cache[1]) {
+      let index = findUnicodeRangeIndex(cp, word_ranges);
+      if (index < 0) {
+        return 0;
+      }
+      let range = word_ranges[index];
+      cache[0] = range[0];
+      cache[1] = range[1];
+      cache[2] = range[2];
+    }
+    return cache[2];
+  }
+}
